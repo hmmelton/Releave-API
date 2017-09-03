365c29626f6ea3756c4a9a49f3f462e5fb1f992a
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-03 10:52:21 -0700

    testing new user creation when login finds no matching user

--- api/controllers/releaveController.js
+++ api/controllers/releaveController.js
@@ -21,17 +21,29 @@ exports.check_api_key = function(req, res, next) {
 // This function logs in a user
 exports.login = function(req, res) {
 	// Make sure fb_id parameter has been passed
-	if (!req.query.fb_id) {
-		res.status(401).json({ error: 'Query parameter fb_id is required.' });
-		return;
-	}
+	if (!req.query.fb_id) res.status(401).json({ error: 'Query parameter fb_id is required.' });
+
 	// Find user with matching facbook_id
 	User.findOne({ 'facebook_id': req.query.fb_id }, function(err, user) {
-		// There was an error
-		if (err) res.status(404).send(err);
+		// Handle error
+		if (err) res.status(500).send(err);
 
-		if (user === null) res.status(404).json({ error: 'User not found' });
-		
+		// If user is null, create new one
+		if (user === null) create_user(req, res);
+
+		// If user was found, return data
 		res.status(200).json(user);
+	});
+};
+
+// This function creates a user
+exports.create_user = function(req, res) {
+	// If user is null, create a new user
+	var new_user = new User(req.body);
+	new_user.save(function(err, user) {
+		// Handle error
+		if (err) res.status(500).send(err);
+
+		res.status(201).json(user);
 	});
 };
