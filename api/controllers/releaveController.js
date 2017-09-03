33480e0d88f8bac92e2b77965ccf6e050e4c7102
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-03 12:16:47 -0700

    reformatted logic in endpoint functions

--- api/controllers/releaveController.js
+++ api/controllers/releaveController.js
@@ -21,21 +21,23 @@ var check_api_key = function(req, res, next) {
 // This function logs in a user
 var login = function(req, res) {
 	// Make sure fb_id parameter has been passed
-	if (!req.query.fb_id) res.status(400).json({ error: 'Query parameter fb_id is required.' });
+	if (!req.query.fb_id) {
+		res.status(400).json({ error: 'Query parameter fb_id is required.' });
+		return;
+	}
 
 	// Find user with matching facbook_id
 	User.findOne({ 'facebook_id': req.query.fb_id }, function(err, user) {
-		// Handle error
-		if (err) res.status(500).send(err);
-
-		// If user is null, create new one
-		if (user === null) {
+		if (err) {
+			// Handle error
+			res.status(500).send(err);
+		} else if (user === null) {
+			// If user is null, create new one
 			create_user(req, res);
-			return;
+		} else {
+			// If user was found, return data
+			res.status(200).json(user);
 		}
-
-		// If user was found, return data
-		res.status(200).json(user);
 	});
 };
 
@@ -46,50 +48,60 @@ var login = function(req, res) {
 // This function fetches a user
 var get_user = function(req, res) {
 	// Make sure id parameter has been passed
-	if (!req.query.id) res.status(400).json({ error: 'Query parameter id is required' });
+	if (!req.query.id) {
+		res.status(400).json({ error: 'Query parameter id is required' });
+		return;
+	}
 
 	// Find user with matching ID
 	User.findById(req.query.id, function(err, user) {
-		// If there was an error, send it back to the client
-		if (err) res.status(500).send(err);
-
-		// If user is null, send back error to client
-		if (user === null) res.status(404).json({ error: 'User not found' });
-
-		// User was found - return to client
-		res.status(200).send(user);
+		if (err) {
+			// If there was an error, send it back to the client
+			res.status(500).send(err);
+		} else if (user === null) {
+			// If user is null, send back error to client
+			res.status(404).json({ error: 'User not found' });
+		} else {
+			// User was found - return to client
+			res.status(200).send(user);
+		}
 	});
 };
 
 // This function creates a user
 var create_user = function(req, res) {
-	// If user is null, create a new user
 	var new_user = new User(req.body);
 	new_user.save(function(err, user) {
 		// If there was an error, send it back to the client
-		if (err) res.status(500).send(err);
-
-		res.status(201).json(user);
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			// User was created - return to client
+			res.status(201).json(user);
+		}
 	});
 };
 
 // This function deletes a user
 var delete_user = function(req, res) {
 	// Make sure id parameter has been passed
-	if (!req.query.id) res.status(400).json({ error: 'Query parameter id is required' });
+	if (!req.query.id) {
+		res.status(400).json({ error: 'Query parameter id is required' });
+		return;
+	}
 
 	// Delete user
 	User.findByIdAndRemove(req.query.id, function(err, user) {
-		// If there was an error, send it back to the client
-		if (err) res.status(500).send(err);
-
-		console.log(user);
-
-		// If user is null, send back error to client
-		if (user === null) res.status(404).json({ error: 'User not found' });
-
-		// User was found and deleted - return to client
-		res.status(200).json({ message: 'User successfully deleted' });
+		if (err) {
+			// If there was an error, send it back to the client
+			res.status(500).send(err);
+		} else if (user === null) {
+			// If user is null, send back error to client
+			res.status(404).json({ error: 'User not found' });
+		} else {
+			// User was found and deleted - return to client
+			res.status(200).json({ message: 'User successfully deleted' });
+		}
 	});
 }
 
