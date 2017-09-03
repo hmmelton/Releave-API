b5e309c097312807621b564f919e7d131b5f174b
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-03 00:19:32 -0700

    changed formate of private strings file

--- private_strings.js
+++ private_strings.js
@@ -1,3 +1,5 @@
-const API_KEY = "4E4F6811-AFF6-4B54-9B27-E47799BE1C55";
-const MONGO_DB = "hmmelton:Lampard#8@ds064198.mlab.com:64198/releave";
-const STRIPE_SECRET_KEY = "sk_test_SHJopqGq01amSfYJge5ORBTG";
+module.exports = {
+	API_KEY: "4E4F6811-AFF6-4B54-9B27-E47799BE1C55",
+	MONGO_DB: "hmmelton:Lampard#8@ds064198.mlab.com:64198/releave",
+	STRIPE_SECRET_KEY: "sk_test_SHJopqGq01amSfYJge5ORBTG"
+};
