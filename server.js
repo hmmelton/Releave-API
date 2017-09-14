0f4c1cd6f6e1aecf73d174712dc22ec347b621fa
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-13 17:51:28 -0700

    removed Passport ref from server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	User = require('./api/models/userModel'), // User model loading
 	Restroom = require('./api/models/restroomModel'), // Restroom model loading
-	passportConfig = require('./passport')
 	strings = require('./private_strings'), // File holding secret strings
 	bodyParser = require('body-parser');
 
@@ -13,8 +12,6 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://' + strings.MONGO_DB, {
 	useMongoClient: true
 });
-
-passportConfig()
 
 app.use(bodyParser.urlencoded({ extended: true })); // Body parser for URL-encoded forms
 app.use(bodyParser.json()); // Body parser for JSON
